48c3c3aefa662c7aa0f2bd446bfc3c6a4691099d
Matt <matt@mjman.net>
2017-11-16 23:21:22 +1100

    start html5 report charts

--- controllers/backend_chartdata.php
+++ controllers/backend_chartdata.php
@@ -4,14 +4,23 @@
 	{
 		public function unique_visits()
 		{
-			$this->xmlData();
-			$this->layout = null;
+			$data = self::get_unique_visits_data();
 
-			$start = $this->get_interval_start(true);
-			$end = $this->get_interval_end(true);
+			$this->viewData['sales_data'] = $data['sales'];
+			$this->viewData['chart_data'] = $data['visits'];
+		}
 
-			$this->viewData['sales_data'] = Shop_Orders_Report::get_totals_chart_data($start, $end);
-			$this->viewData['chart_data'] = Cms_Analytics::getVisitorsChartData($start, $end);
+		public static function get_unique_visits_data(){
+			$_this = new Backend_ChartData();
+			$start = $_this->get_interval_start(true);
+			$end = $_this->get_interval_end(true);
+
+			$data = array(
+				'sales' => Shop_Orders_Report::get_totals_chart_data($start, $end),
+				'visits' => Cms_Analytics::getVisitorsChartData($start, $end),
+			);
+
+			return $data;
 		}
 	}
 
